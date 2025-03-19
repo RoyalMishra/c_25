4a1129a89a8822eea1c2b22bbb51484e8a71a79d
royal <royalmishr@gmail.com>
2025-03-19 19:40:00 +0530

    Final Commit to the Areas

---
+++ #02_c_25/Area.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main()
+{
+    int s1,r1,r2;
+        // Area of a Square 
+
+         printf("Enter the Length of Side of Square : \t");
+         scanf("%d",&s1);
+
+         printf("The Area of the Square is : %d",s1*s1);
+
+        // Area of Rectangle
+
+         printf("Enter the Length of Rectangle : \t");
+         scanf("%d",&r1);
+
+         printf("Enter the breadth of Rectangle : \t");
+         scanf("%d",&r2);
+         
+         printf("The Area of the Rectangle is : %d",r1*r2);
+         return 0;
+}
