68975f559452d238dfdfd67cede02fc164e8762a
royal <royalmishr@gmail.com>
2025-03-19 18:53:01 +0530

    Day_02_New Files Added

---
+++ #02_c_25/input_from_User.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    /* This will be taking Input From the Use Using -- Scanf() -- Function*/
+    char name;
+    scanf("%c",&name);
+    printf("\n Hello Welcome Mr. %c",name);
+
+    /* Escape Sequesnce Characters in C */
+
+    /*  /n --> To Print output to a new line 
+        /t --> To Print output to a Tab
+        /' --> To print the ' single inverted comman in code output
+        /" --> To print the " double inverted comman in code output
+
+    */
+}
---
+++ #02_c_25/var_and_values.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{   /* Integer Assignment */
+        
+        int a=10;
+        int b=20;
+        int c=a+b;
+
+        printf(" The Value of C is : %d",c);
+
+     // Float assignment
+
+        float a1=10.2;
+        float b1=20.4;
+        float c1=a1+b1;
+
+        printf("\n The Value of C is : %f",c1);
+
+    //  char assignment
+
+         char a_char='T';
+         char b_char='h';
+        
+ 
+         printf("\n %c",a_char);
+         printf("\n %c",b_char);
+        
+        
+
+        return 0;
+}
