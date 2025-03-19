c8e46d124aab1f024a3c19cee4c222edcbb4f2ae
royal <royalmishr@gmail.com>
2025-03-19 17:59:03 +0530

    Day One finished

--- helloworld.c
+++ helloworld.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 int main()
-{
-    printf("Hello World");
-    printf("\n This is the Second Line of code");
-    return (0);
+{       /* This is the first Progam for the Hello World  --18/03/2025-- */
+    printf("Hello World");                                                  /* This is used to produce the output to the screen */ 
+    printf("\n This is the Second Line of code");                           /* Use of next Line Keyword */
+    printf("\n the Size of the Integer in memory is: %d",sizeof(int));      /* Use of the Size of funtion/operator */
+    printf("\n the Size of the float in memory is: %d",sizeof(float));
+    printf("\n the Size of the string in memory is: %d",sizeof(char));
+    printf("\n the Size of the long in memory is: %d",sizeof(long));
+    return 0;                                                           /* If all ok return nothing ie 0 interger */
+
 }
-
-
-
-
-
